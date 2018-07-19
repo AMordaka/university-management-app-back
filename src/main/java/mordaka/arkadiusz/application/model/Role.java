3b9fd5a3d8a6d789eb30a9fe994f46aa72ee7f12
Arkadiusz Mordaka <mordakaa@dc.emersondc.pl>
2018-07-19 09:22:06 +0200

    Dodanie modelu User oraz Role

---
+++ src/main/java/mordaka/arkadiusz/application/model/Role.java
@@ -0,0 +1,22 @@
+package mordaka.arkadiusz.application.model;
+
+
+import lombok.Data;
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    @Column(length = 60)
+    private RoleName name;
+
+}
---
+++ src/main/java/mordaka/arkadiusz/application/model/RoleName.java
@@ -0,0 +1,6 @@
+package mordaka.arkadiusz.application.model;
+
+public enum RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
