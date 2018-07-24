350557e661642c7855d20f998471bf0ec31f509b
Arkadiusz Mordaka <1995.arek.m@gmail.com>
2018-07-24 08:41:32 +0200

    Przeniesienie metod do autentykacji do UserService

--- src/main/java/mordaka/arkadiusz/application/service/UserService.java
+++ src/main/java/mordaka/arkadiusz/application/service/UserService.java
@@ -1,7 +1,15 @@
 package mordaka.arkadiusz.application.service;
 
+import mordaka.arkadiusz.application.payload.JwtAuthenticationResponse;
+import mordaka.arkadiusz.application.payload.LoginRequest;
+import mordaka.arkadiusz.application.payload.SignUpRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface UserService {
+
+    JwtAuthenticationResponse authenticateUser(LoginRequest request);
+
+    ResponseEntity<?> registerUser(SignUpRequest signUpRequest);
 }
