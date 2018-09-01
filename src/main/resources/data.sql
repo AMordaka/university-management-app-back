fabc661b5769e358a7ffdacafd4ef7301f0f12d7
Arkadiusz Mordaka <arkadiusz1057@gmail.com>
2018-09-01 20:29:46 +0200

    Dodanie roli do bazy

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,20 @@
-INSERT INTO roles(name) VALUES('ROLE_USER');
+INSERT INTO roles(name) VALUES('ROLE_ADMIN');
 INSERT INTO roles(name) VALUES('ROLE_TEACHER');
+INSERT INTO roles(name) VALUES('ROLE_STUDENT');
 
-INSERT INTO USERS(id,created_at,updated_at,city,email,name,number_street,password,postal_code,street,surname,username) VALUES (null, '2018-08-03', '2018-08-03', 'Wolborz','arek@wp.pl','Arek','100','password','97-110','Lodzka','Kowalski','200200');
+INSERT INTO USERS(id,created_at,updated_at,city,email,name,number_street,password,postal_code,street,surname,username) VALUES (null, '2018-08-03', '2018-08-03', 'Wolborz','arek@wp.pl','Arek','100','$2a$10$.MZ.hlndzmYM8MYntstnROZDeRlTSbEUPWCEPzD.Tmd0SH8ZlpS2G','97-110','Lodzka','Kowalski','100100');
 INSERT INTO STUDENT(id,user_id) VALUES (null,'1');
 
-INSERT INTO USERS(id,created_at,updated_at,city,email,name,number_street,password,postal_code,street,surname,username) VALUES (null, '2018-08-03', '2018-08-03', 'Moszczenica','mail@wp.pl','Tomasz','100','password','97-110','Lodzka','Kowalski','150000');
+INSERT INTO USERS(id,created_at,updated_at,city,email,name,number_street,password,postal_code,street,surname,username) VALUES (null, '2017-02-04', '2018-08-03', 'Moszczenica','mail@wp.pl','Tomasz','100','$2a$10$.MZ.hlndzmYM8MYntstnROZDeRlTSbEUPWCEPzD.Tmd0SH8ZlpS2G','97-110','Lodzka','Kowalski','100101');
 INSERT INTO TEACHER(id,user_id) VALUES (null,'2');
 
-INSERT INTO USERS(id,created_at,updated_at,city,email,name,number_street,password,postal_code,street,surname,username) VALUES (null, '2018-08-03', '2018-08-03', 'Moszczenica','mail2@wp.pl','Tomasz','100','password','97-110','Lodzka','Kowalski','150001');
+INSERT INTO USERS(id,created_at,updated_at,city,email,name,number_street,password,postal_code,street,surname,username) VALUES (null, '2018-09-18', '2018-08-03', 'Moszczenica','mail2@wp.pl','Tomasz','100','$2a$10$.MZ.hlndzmYM8MYntstnROZDeRlTSbEUPWCEPzD.Tmd0SH8ZlpS2G','97-110','Lodzka','Kowalski','100102');
 INSERT INTO TEACHER(id,user_id) VALUES (null,'3');
 
 INSERT INTO ITEM(id,created_at,updated_at,grade,subject_name,student_id,teacher_id) VALUES (null,'2018-08-03','2018-08-03','4','Informatyka','1','1');
 INSERT INTO ITEM(id,created_at,updated_at,grade,subject_name,student_id,teacher_id) VALUES (null,'2018-08-03','2018-08-03','4','Matematyka','1','1');
 INSERT INTO ITEM(id,created_at,updated_at,grade,subject_name,student_id,teacher_id) VALUES (null,'2018-08-03','2018-08-03',null,'WF','1','2');
 
+INSERT INTO USER_ROLES(user_id,role_id) VALUES('1','3');
+INSERT INTO USER_ROLES(user_id,role_id) VALUES('2','2');
+INSERT INTO USER_ROLES(user_id,role_id) VALUES('3','1');
